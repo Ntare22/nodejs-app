ac4d2450644bbd974805ecb7236b50b20b4c1f87
Ntare22 <jim.musika2016@kepler.org>
2019-11-30 21:42:26 +0200

    installed express & started with the basics

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const http = require('http');
-const routes = require('./routes');
 
-const server = http.createServer(routes.handler);
+const express = require('express');
+
+const app = express();
+
+ 
+app.use((req, res, next) => {
+  console.log('In another middleware');
+  res.send('<h1>Hello from Express</h1>');
+});
+const server = http.createServer(app);
 
 server.listen(3000);
