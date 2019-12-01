1eb5fc43b31486b4bca7dd85f55a08d5bf8ac95f
Ntare22 <jim.musika2016@kepler.org>
2019-12-01 14:30:19 +0200

    completed expressjs basics

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
-const http = require('http');
-
 const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path')
+
 
 const app = express();
 
- 
-app.use((req, res, next) => {
-  console.log('In another middleware');
-  res.send('<h1>Hello from Express</h1>');
-});
-const server = http.createServer(app);
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-server.listen(3000);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, 'views', 'not-found.html'));
+})
+
+
+app.listen(3000);
