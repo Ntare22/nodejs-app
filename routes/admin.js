6ab4f8980548c3517d20455d44592b9c5ec48ff7
Ntare22 <jim.musika2016@kepler.org>
2019-12-03 11:15:56 +0200

    intro to using templating engines

--- routes/admin.js
+++ routes/admin.js
@@ -5,6 +5,8 @@ const rootDir = require('../util/path');
 
 const router = express.Router();
 
+const products = [];
+
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
   res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
@@ -12,8 +14,9 @@ router.get('/add-product', (req, res, next) => {
 
 // /admin/product => POST
 router.post('/add-product', (req, res, next) => {
-  console.log(req.body);
+  products.push({ title: req.body.title });
   res.redirect('/');
 })
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;
