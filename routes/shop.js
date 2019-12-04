21cb624898871a05e1a9fc05e19008e98a5ea4f9
Ntare22 <jim.musika2016@kepler.org>
2019-12-04 15:42:20 +0200

    introduced to the model view controller file project file arrangement

--- routes/shop.js
+++ routes/shop.js
@@ -1,15 +1,8 @@
-const path = require('path')
 const express = require('express');
-
-const rootDir = require('../util/path');
-const adminData = require('./admin');
-
+const productsController = require('../controllers/products');
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  const products = adminData.products;
-  res.render('shop', { prods: products, path: '/', pageTitle: 'Shop' });
-});
+router.get('/', productsController.getProducts);
 
-module.exports = router;   
+module.exports = router;    
